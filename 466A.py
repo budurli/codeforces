c376ca82d5cf5157b01c88f8f68a6d7620c220ee
Maxim Smirnoff <smirnoffmg@gmail.com>
2016-01-14 13:35:21 +0300

    14/01/2016 @8min

--- 466A.py
+++ 466A.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 n, m, a, b = map(int, raw_input().split(' '))
-print(min(n * a, round(n / float(m)) * b))
+
+profit = m * a - b
+if profit > 0:
+    if n % m != 0:
+        overhead = (n / m + 1) * b
+        exact = (n / m) * b + (n % m) * a
+        print(min(overhead, exact))
+    else:
+        print(n / m * b)
+else:
+    print(n * a)
