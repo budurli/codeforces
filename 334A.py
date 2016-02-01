f2dcfa73bde68c5fbbee910669e0450fb95dedc9
Maxim Smirnoff <smirnoffmg@gmail.com>
2016-02-01 10:24:11 +0300

    02.01.2016

--- 334A.py
+++ 334A.py
@@ -3,5 +3,9 @@ n = int(raw_input())
 
 candies = range(1, n ** 2 + 1)
 
-for i in xrange(n):
-    print()
+for i in xrange(0, n ** 2, n):
+    row = []
+    for j in xrange(i, i + n / 2):
+        row.extend([candies[j], candies[n ** 2 - (j + 1)]])
+
+    print(' '.join(map(str, row)))
