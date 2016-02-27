fe9026f28d24acfcecfb8d656200c0ec090af87e
Maxim Smirnov <smirnoffmg@gmail.com>
2016-02-27 12:59:10 +0300

    27/02/2016

--- 4C.py
+++ 4C.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
+
+n = int(raw_input())
+
+names = defaultdict(list)
+result = []
+for i in xrange(n):
+    name = raw_input()
+    if len(names[name]) > 0:
+        new_name = '{}{}'.format(name, len(names[name]))
+        names[name].append(new_name)
+        result.append(new_name)
+    else:
+        names[name].append(name)
+        result.append('OK')
+
+for r in result:
+    print(r)
